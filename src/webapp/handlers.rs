4e3eebc73321e610d764a67335909170b70a0587
Alexander Ou <alexou@femtodata.com>
2025-09-11 19:31:10 -0700

    remove stale comment block

--- src/webapp/handlers.rs
+++ src/webapp/handlers.rs
@@ -85,18 +85,6 @@ pub async fn get_index(
 ) -> Result<Html<String>, WebappError> {
     let template = env.get_template("home")?;
 
-    // let df = get_df_from_db_table("vmware")?;
-
-    // let df = df
-    //     .lazy()
-    //     .with_columns(vec![
-    //         (col("notification")
-    //             .dt()
-    //             .quarter()
-    //             .alias("notification_quarter")),
-    //     ])
-    //     .collect()?;
-
     let rendered = template.render(context! {
         title => "Home",
         content => "STUFF GOES HERE",
